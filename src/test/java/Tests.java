12e89ed0bd0fa1e3c22d16b03979b5b41e444017
Ela <ela@testelka.pl>
2023-02-13 15:35:02 +0100

    findElement i findElements examples

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,12 +2,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 public class Tests {
 
@@ -26,14 +31,7 @@ public class Tests {
     public void exampleTest() {
         String url = "http://localhost:8080/";
         driver.get(url);
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(url, driver.getCurrentUrl(),
-                        "URL address of the main page is not what expected."),
-                () -> Assertions.assertEquals("Test App – Just another WordPress site",
-                        driver.getTitle(), "Site title is not correct.")
-        );
-
-
-
+        WebElement searchField = driver.findElement(By.id("wc-block-search__input-1"));
+        List<WebElement> addToCartButtons = driver.findElements(By.className("add_to_cart_button"));
     }
 }
