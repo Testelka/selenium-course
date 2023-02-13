88cd50c07810734676f1ba8025345910a30fdc69
Ela <ela@testelka.pl>
2023-02-13 14:01:11 +0100

    Selenium library added and the first test written.

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -2,15 +2,27 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tests {
 
+    WebDriver driver;
     @BeforeEach
-    public void setup() {}
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
     @AfterEach
-    public void teardown() {}
+    public void quitDriver() {
+        driver.quit();
+    }
+
     @Test
-    public void test() {
-        Assertions.assertEquals("a", "a");
+    public void exampleTest() {
+        String url = "http://localhost:8080/";
+        driver.get(url);
+        Assertions.assertEquals(url, driver.getCurrentUrl(),
+                "URL address of the main page is not what expected.");
     }
 }
