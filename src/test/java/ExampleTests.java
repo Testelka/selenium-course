7520fbca55329803edb9ca51d494c3f8ecd9a56b
Ela Sądel <ela@testelka.pl>
2023-02-16 18:32:08 +0100

    WebDriverWait added

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
@@ -10,19 +13,36 @@ import java.util.List;
 public class ExampleTests {
 
     WebDriver driver;
+    WebDriverWait wait;
     @BeforeEach
     public void setup() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-
-        //Duration implicitWait = driver.manage().timeouts().getImplicitWaitTimeout();
-        //Duration pageLoadTimeout = driver.manage().timeouts().getPageLoadTimeout();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @AfterEach
     public void quitDriver() {
         driver.quit();
+    }
+
+    @Test
+    public void update_quantity_in_cart_should_update_total_price() {
+        driver.get("http://localhost:8080/product/" +
+                "the-elements-of-qualitative-chemical-analysis-vol-1-parts-1-and-2-by-stieglitz/");
+        driver.findElement(By.name("add-to-cart")).click();
+        driver.get("http://localhost:8080/cart/");
+        WebElement quantityField = driver.findElement(By.className("qty"));
+        quantityField.clear();
+        quantityField.sendKeys("2");
+        driver.findElement(By.name("update_cart")).click();
+
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.className("blockUI"), 0));
+
+        WebElement total = driver.findElement(By.className("order-total"));
+
+        Assertions.assertEquals("Total 28,00 €", total.getText(),
+                "Total price is not correct.");
     }
 
     @Test
@@ -32,7 +52,10 @@ public class ExampleTests {
         WebElement addToCartButton = driver.findElement(By.name("add-to-cart"));
         addToCartButton.click();
         driver.findElement(By.className("wc-block-mini-cart__button")).click();
-        WebElement totalPrice = driver.findElement(By.className("wc-block-components-totals-item__value"));
+
+        WebElement totalPrice = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.className("wc-block-components-totals-item__value"))); 
+
         Assertions.assertEquals("12,00 €", totalPrice.getText(),
                 "The price displayed in minicart is not correct.");
     }
