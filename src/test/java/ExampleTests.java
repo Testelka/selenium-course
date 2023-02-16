3dfc416e9f50645b9262069572aa97134f046c78
Ela Sądel <ela@testelka.pl>
2023-02-16 11:50:32 +0100

    Simple adding to cart test added

---
+++ src/test/java/ExampleTests.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class ExampleTests {
+
+    WebDriver driver;
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void quitDriver() {
+        driver.quit();
+    }
+
+    @Test
+    public void addToCartTest() {
+        driver.get("http://localhost:8080/product/" +
+                "a-popular-history-of-astronomy-during-the-nineteenth-century-by-agnes-m-clerke/");
+        WebElement addToCartButton = driver.findElement(By.name("add-to-cart"));
+        addToCartButton.click();
+        WebElement miniCart = driver.findElement(By.className("wc-block-mini-cart__amount"));
+        Assertions.assertEquals("12,00 €", miniCart.getText(),
+                "The price displayed in minicart is not correct.");
+    }
+
+    @Test
+    public void loginTest() {
+
+    }
+}
