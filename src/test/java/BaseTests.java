6b2f301450a8e5f6bc26cae709d3d7da8ae99935
Ela Sądel <ela@testelka.pl>
2023-02-22 21:02:34 +0100

    Headless examples added

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -2,8 +2,6 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -16,7 +14,18 @@ public class BaseTests {
 
     @BeforeEach
     public void setup() {
-        driver = new FirefoxDriver();
+        //FirefoxOptions options = new FirefoxOptions();
+        //options.addArguments("-headless");
+        //driver = new FirefoxDriver(options);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
+
+        //EdgeOptions options = new EdgeOptions();
+        //options.addArguments("--headless=new");
+        //driver = new EdgeDriver(options);
+
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
