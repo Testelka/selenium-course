c831498cce86028fb3f11fbfe3aaaa70a2071eeb
Ela Sądel <ela@testelka.pl>
2023-03-13 12:28:59 +0100

    Shadow DOM for Firefox example

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,7 +12,7 @@ public class BaseTests {
 
     @BeforeEach
     public void setup() {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,19 +1,24 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ExampleTests extends BaseTests{
     @Test
-    public void shadow_DOM_example_chrome_edge_test() {
+    public void shadow_DOM_example_firefox_test() {
         driver.get("https://fakestore.testelka.pl/shadow-dom-w-selenium/");
 
         WebElement shadowHost = driver.findElement(By.cssSelector("#host"));
-        SearchContext shadowRoot = shadowHost.getShadowRoot();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        List<?> elements = (List<?>) js.executeScript(
+                "return arguments[0].shadowRoot.children", shadowHost);
+        WebElement shadowRoot = (WebElement) elements.get(0);
+
         shadowRoot.findElement(By.cssSelector("#input")).sendKeys("Halko");
         shadowRoot.findElement(By.cssSelector("button")).click();
-
         String displayedText = shadowRoot.findElement(By.cssSelector("#output")).getText();
 
         Assertions.assertEquals("Wprowadzony tekst: Halko", displayedText,
