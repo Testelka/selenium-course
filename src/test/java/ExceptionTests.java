de97b4accaab7c06596e509166bd7126bc2ea66e
Ela Sądel <ela@testelka.pl>
2023-03-14 16:36:33 +0100

    ElementNotInteractableException example

--- src/test/java/ExceptionTests.java
+++ src/test/java/ExceptionTests.java
@@ -1,18 +1,11 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class ExceptionTests extends BaseTests {
 
     @Test
-    public void no_product_reviews_should_display_no_reviews_message() {
-        driver.get("https://fakestore.testelka.pl/product/windsurfing-w-lanzarote-costa-teguise/");
-        driver.findElement(By.cssSelector(".woocommerce-store-notice__dismiss-link")).click();
-        driver.findElement(By.cssSelector("#tab-title-reviews a")).click();
-
-        Assertions.assertDoesNotThrow(() -> driver.findElement(By.cssSelector(".woocommerce-noreviews")),
-                "The no reviews message doesn't exist.");
+    public void element_not_interactable_exception_example() {
+        driver.get("http://localhost:8080/product/how-to-do-chemical-tricks-by-a-anderson/");
+        driver.findElement(By.cssSelector("#submit")).click();
     }
-
-
 }
