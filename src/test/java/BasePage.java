a4b7b93d3d9cd94dfeddf234ba26e15d81047b62
Ela Sądel <ela@testelka.pl>
2023-03-16 16:08:06 +0100

    BasePage

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.WebDriver;
 
-public class BasePage {
+public abstract class BasePage {
     protected final WebDriver driver;
     protected BasePage(WebDriver driver) {
         this.driver = driver;
