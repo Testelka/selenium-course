36719520df2bb29c08c859682172f79a370f10e8
Ela Sądel <ela@testelka.pl>
2023-03-17 11:14:36 +0100

    Browser switch

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,39 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BaseTest {
-    WebDriver driver;
+    protected WebDriver driver;
+    private final String browser = "firefox";
+    private final boolean isHeadless = true;
     @BeforeEach
     public void setup() {
-        driver = new FirefoxDriver();
+        if (browser.equals("firefox")) {
+            if (isHeadless) {
+                driver = new FirefoxDriver(new FirefoxOptions().addArguments("-headless"));
+            } else {
+                driver = new FirefoxDriver();
+            }
+        } else if (browser.equals("chrome")) {
+            if (isHeadless) {
+                driver = new ChromeDriver(new ChromeOptions().addArguments("--headless=new"));
+            } else {
+                driver = new ChromeDriver();
+            }
+        } else if (browser.equals("edge")) {
+            if (isHeadless) {
+                driver = new EdgeDriver(new EdgeOptions().addArguments("--headless=new"));
+            } else {
+                driver = new EdgeDriver();
+            }
+
+        }
     }
     @AfterEach
     public void quitDriver() {
